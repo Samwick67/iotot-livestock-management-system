65634e528675874582b487ec503d09b1bd119f01
Samuel Naidoo <naidoosamuel67@gmail.com>
2026-08-31 22:21:46 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,2 @@
 // db.js
-const { Pool } = require('pg');
-
-// Change these values to your PostgreSQL credentials
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'animaltrack',
-    password: 'sam',
-    port: 5432,
-});
-
-module.exports = pool;
+const { Pool } = require('pg'); const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }); module.exports = pool;
